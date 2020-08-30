b4a48a3c06e6fc44e3d5e299d240fee5d033a7a0
Gabriel Madeira Camargo <gabrielcamargom07@gmail.com>
2020-08-30 18:01:04 -0300

    pong-3
    “The Paddle Update”

--- main.lua
+++ main.lua
@@ -6,10 +6,14 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load() -- Inicia o gamestate no começo da execução do programa, equivalente ao método construtor do Java ou Create do GameMaker.
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     smallFont = love.graphics.newFont('font.ttf', 8)
+
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     love.graphics.setFont(smallFont)
 
@@ -19,26 +23,56 @@ function love.load() -- Inicia o gamestate no começo da execução do programa,
         resizable = false,
         vsync = true -- Sincronizado com a taxa de atualização do monitor.
     })
-end -- COLOCAR END E FUNCTION NAS FUNÇÕES
--- love.update(dt) Executado a cada frame usando o delta time. Equivalente ao Tick do Java e Step do GameMaker.
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT-50
+end --COLOCAR END E FUNCTION NAS FUNÇÕES
+
 
 function love.draw() -- Também executado a cada frame, essa função desenha na tela. Equivalente ao Render do Java e Draw do GameMaker.
     push:apply('start')
+
+    love.graphics.clear(40, 45, 52, 255)
+
     love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 100, VIRTUAL_WIDTH, 'center')
 
-    --love.graphics.clear(40, 45, 52, 255)
-
     -- Primeira raquete
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- Segunda raquete (Inimigp)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     -- Bola
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
     push:apply('end')
 
+end
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
 end
 
 function love.keypressed(key) if key == 'escape' then love.event.quit() end end
@@ -80,4 +114,15 @@ não precisa usar neste projeto específico, já que quase tudo é branco, a cor
 ‘Preencher’ ou ‘linha’, que resulta em um retângulo preenchido ou não-preebcgudi, respectivamente, e os outros quatro parâmetros são seus
 posição e dimensões de tamanho. Esta é a função de desenho fundamental de toda a nossa implementação Pong!
 
+]]
+
+--[[    Funções importantes pong -3
+
+ love.keyboard.isDown (chave)
+
+-Retorna verdadeiro ou falso dependendo se a tecla especificada está pressionada; é diferente de
+love.keypressed (key) em que isso pode ser chamado arbitrariamente e retornará continuamente verdadeiro se a tecla for pressionada,
+onde love.keypressed (key) só disparará seu código uma vez sempre que a tecla for pressionada inicialmente. No entanto, já que nós
+queremos ser capazes de mover nossas pás para cima e para baixo segurando as teclas apropriadas, precisamos de uma função para testar
+períodos mais longos de entrada, portanto, o uso de love.keyboard.isDown (tecla)!
 ]]

5f80d41bcba63ec275954017dbb3dec31ed31e97
Gabriel Madeira Camargo <gabrielcamargom07@gmail.com>
2020-08-30 19:14:29 -0300

    pong-4
    “The Ball Update”

--- main.lua
+++ main.lua
@@ -11,6 +11,8 @@ PADDLE_SPEED = 200
 function love.load() -- Inicia o gamestate no começo da execução do programa, equivalente ao método construtor do Java ou Create do GameMaker.
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    math.randomseed(os.time())
+
     smallFont = love.graphics.newFont('font.ttf', 8)
 
     scoreFont = love.graphics.newFont('font.ttf', 32)
@@ -29,16 +31,28 @@ function love.load() -- Inicia o gamestate no começo da execução do programa,
 
     player1Y = 30
     player2Y = VIRTUAL_HEIGHT-50
+
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+
+    ballDX = math.random(2) == 1 and 100 or - 100
+    ballDY = math.random(-50,50)
+    --Abreviações de delta time.
+
+    gameState = 'start'
 end --COLOCAR END E FUNCTION NAS FUNÇÕES
 
 
 function love.draw() -- Também executado a cada frame, essa função desenha na tela. Equivalente ao Render do Java e Draw do GameMaker.
     push:apply('start')
 
-    love.graphics.clear(40, 45, 52, 255)
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 100, VIRTUAL_WIDTH, 'center')
-
+    if gameState == 'play' then
+    love.graphics.printf('Crazy Pong!', 0, VIRTUAL_HEIGHT / 2 - 100, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Paused Pong!', 0, VIRTUAL_HEIGHT / 2 - 100, VIRTUAL_WIDTH, 'center')
+    end
     -- Primeira raquete
     love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
@@ -46,7 +60,7 @@ function love.draw() -- Também executado a cada frame, essa função desenha na
     love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     -- Bola
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    love.graphics.rectangle('fill', ballX, ballY, 4,4)
 
     love.graphics.setFont(scoreFont)
     love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
@@ -59,33 +73,69 @@ function love.update(dt)
     -- player 1 movement
     if love.keyboard.isDown('w') then
         -- add negative paddle speed to current Y scaled by deltaTime
-        player1Y = player1Y + -PADDLE_SPEED * dt
+        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('s') then
         -- add positive paddle speed to current Y scaled by deltaTime
-        player1Y = player1Y + PADDLE_SPEED * dt
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
     end
 
     -- player 2 movement
     if love.keyboard.isDown('up') then
         -- add negative paddle speed to current Y scaled by deltaTime
-        player2Y = player2Y + -PADDLE_SPEED * dt
+        player2Y = math.max(0,player2Y + -PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('down') then
         -- add positive paddle speed to current Y scaled by deltaTime
-        player2Y = player2Y + PADDLE_SPEED * dt
+        player2Y = math.min(VIRTUAL_HEIGHT-20, player2Y + PADDLE_SPEED * dt)
+    end
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
     end
 end
 
-function love.keypressed(key) if key == 'escape' then love.event.quit() end end
 
---[[    Funções importantes pong-0
+function love.keypressed(key) 
+    if key == 'escape' then 
+        love.event.quit() 
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else gameState = 'start'
+
+            ballX = VIRTUAL_WIDTH / 2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50,50) * 1.5
+
+            --[[
+                1. Ele garante que cada vez que você chamar math.random, ele produza um resultado diferente
+                2. equivalente a:
+                se math.random (2) == 1 então
+                        ballDX = 100
+                            else
+                        ballDX = -100
+                onde math.random (2) dá 1 ou 2
+                3. dá um número inteiro entre -50 e 50
+            --]]
+
+        end
+    end
+end
+
+--Fim do código, comentários abaixo
+
+--[[
+    
+    Funções importantes pong-0
 
 love.graphics.printf() É equivalente ao System.out.println(); do Java ou show_debug_message(), mas permite desenhar fisicamente na tela.
 
 love.window.setMode() É usado pra inicializar as dimensões da janela e setar parâmetros como vsync, fullscren, redimensionar a janela, etc.
 
---]]
 
---[[    Funções importantes pong-1
+
+    Funções importantes pong-1
 
  love.graphics.setDefaultFilter (min, mag)  Define o filtro de escala de textura ao minimizar e ampliar texturas e fontes; o padrão é bilinear, o que causa
 embaçamento e, para nossos casos de uso, normalmente queremos a filtragem do vizinho mais próximo ('mais próximo'), o que resulta em perfeita
@@ -96,9 +146,9 @@ main.lua, na mesma linha de love.load (), love.update (dt) e love.draw ().
 ● love.event.quit ()
 -Função simples que encerra o aplicativo.
 
---]]
 
---[[    Funções importantes pong-2
+
+    Funções importantes pong-2
 
 love.graphics.newFont (caminho, tamanho) Define uma fonte nova
 
@@ -114,9 +164,9 @@ não precisa usar neste projeto específico, já que quase tudo é branco, a cor
 ‘Preencher’ ou ‘linha’, que resulta em um retângulo preenchido ou não-preebcgudi, respectivamente, e os outros quatro parâmetros são seus
 posição e dimensões de tamanho. Esta é a função de desenho fundamental de toda a nossa implementação Pong!
 
-]]
 
---[[    Funções importantes pong -3
+
+    Funções importantes pong-3
 
  love.keyboard.isDown (chave)
 
@@ -125,4 +175,20 @@ love.keypressed (key) em que isso pode ser chamado arbitrariamente e retornará 
 onde love.keypressed (key) só disparará seu código uma vez sempre que a tecla for pressionada inicialmente. No entanto, já que nós
 queremos ser capazes de mover nossas pás para cima e para baixo segurando as teclas apropriadas, precisamos de uma função para testar
 períodos mais longos de entrada, portanto, o uso de love.keyboard.isDown (tecla)!
-]]
+
+    Funções importantes pong-4
+math.randomseed(num)
+- ”Planta" o gerador de números aleatórios usado por Lua (math.random) com algum valor tal que sua aleatoriedade é
+dependente desse valor fornecido, o que nos permite passar em números diferentes a cada jogada para garantir
+inconsistência em diferentes execuções de programas (ou uniformidade se quisermos um comportamento consistente para teste).
+os.time ()
+Função -Lua que retorna, em segundos, o horário desde 00:00:00 UTC de 1º de janeiro de 1970, também conhecido como Unix epoch time
+(https://en.wikipedia.org/wiki/Unix_time).
+math.random (min, max)
+-Retorna um número aleatório, dependente do gerador de número aleatório propagado, entre o mínimo e o máximo,
+inclusive.
+math.min (num1, num2)
+-Retorna o menor dos dois números passados.
+math.max (num1, num2)
+-Retorna o maior dos dois números passados.
+--]]
